aae9c0e9d56c154d44c1e442948220fa96ac999b
Foteini Kounavi <foteinikounavi@Foteinis-Air.home>
2025-05-26 10:56:38 +0300

    remove-example

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,5 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('https://www.novibet.gr/live-programma')
-//     cy.get('[data-cy="closeBtn"]').click();
-//     cy.wait(1000)
-//     cy.get('.tutorial_button').click({force:true})
-//    cy.get('.scheduleFilters_sports').click({force:true})
-//    cy.get('.scheduleFilters_dropdownList .scheduleFilters_dropdownListSportItem').eq(1).click({force:true})
-   
-  
-//   })
-// })
+
+
 
 describe('novibet task1',() => {
   beforeEach(() => {
